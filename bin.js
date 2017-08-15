ee61d86f2c31a56c3bb718beca02cb56a564a2eb
alligator-io <markus@sonium.org>
2017-08-15 21:04:24 +0200

    shell command to set bootstrap peer

--- bin.js
+++ bin.js
@@ -7,12 +7,16 @@ var name = process.env.alligator_appname || 'alligator'
 var path = require('path')
 var Peer = require('./')
 
+var argv = require('minimist')(process.argv.slice(2));
+
 var config = require('rc')(name, {
   listen: [
     'shs+tcp://[' + address.ipv4() + ']:4239',
     'shs+ws://[' + address.ipv4() + ']:4238'
   ], path: path.join(home(), '.' + name)
 })
+
+if(argv._.length>0)config.bootstrap = argv._.concat(config.bootstrap||[])
 
 config.info = require('./peerInfo').loadOrCreateSync(path.join(config.path, 'peerInfo'))
 
