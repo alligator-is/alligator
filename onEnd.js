cd136442fdb3b8bd963bb25831a12f02a46280f8
alligator-io <markus@sonium.org>
2017-09-03 18:49:47 +0200

    detect end of duplex stream

---
+++ onEnd.js
@@ -0,0 +1,38 @@
+  
+   module.exports=function onEnd(params, cb) {
+    var source = params.source, sink = params.sink
+    var ended = false, _ended = false
+  
+    var del = function () {
+      if (ended === true && _ended === true) {
+        if(cb)cb()
+          delete cb
+      }
+    }
+
+    params.source = function (end, cb) {
+        source(end, function (end, data) {
+            if (end) ended = true
+            cb(end, data)
+            if (end && del) del()
+        })
+    }
+
+    params.sink = function (read) {
+        sink(function (abort, cb) {
+            if (abort) {
+                _ended = true
+                if (del) del()
+                return read(abort, cb)
+            }
+            read(abort, function (end, data) {
+                cb(end, data)
+                if (end) _ended = true
+                if (end && del) del()
+            })
+        })
+    }
+
+ 
+    return params
+  }
