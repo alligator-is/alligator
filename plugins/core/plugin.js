7a045846947d365f2934f02421a1992c12f5341d
alligator-io <markus@sonium.org>
2020-10-18 20:25:56 +0200

    add call plugin

--- plugins/core/plugin.js
+++ plugins/core/plugin.js
@@ -6,7 +6,4 @@ module.exports[3] = require("./connect")
 module.exports[4] = require("./addrs")
 module.exports[5] = require("./dht")
 module.exports[6] = require("./ping")
-/*
-module.exports[7] = require("./lb")
-
-*/
+module.exports[7] = require("./call")
