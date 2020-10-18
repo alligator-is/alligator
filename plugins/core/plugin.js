9f8e9c26429a4b0eff6772882a63dbb1b8ed24a5
alligator-io <markus@sonium.org>
2020-10-18 11:16:50 +0200

    remove lb

--- plugins/core/plugin.js
+++ plugins/core/plugin.js
@@ -1,3 +1,4 @@
+
 module.exports[0] = require("./groups")
 module.exports[1] = require("./friends")
 module.exports[2] = require("./identities")
@@ -5,4 +6,7 @@ module.exports[3] = require("./connect")
 module.exports[4] = require("./addrs")
 module.exports[5] = require("./dht")
 module.exports[6] = require("./ping")
+/*
 module.exports[7] = require("./lb")
+
+*/
