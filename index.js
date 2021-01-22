f79e0580ec0204ce7a3223ab397e7ae83d6af934
alligator-io <markus@sonium.org>
2021-01-22 23:23:05 +0100

    deps++

--- index.js
+++ index.js
@@ -2,27 +2,6 @@ const { _, Local } = require("icebreaker-rpc")
 
 const api = {}
 
-_.events = ()=>{
-    const notify = _.notify()
-    let queue = []
-    return {
-        emit:(event)=>{
-            if(queue)return queue.push(event)
-            notify(event)
-        },
-        listen:()=>{
-            const source = notify.listen()
-            if(queue){
-                queue.forEach(notify)
-                queue = null
-            }
-          
-            return source
-        },
-        end:notify.end
-    }
-}
-
 api.actions = Local()
 
 module.exports={Action:require("./lib/action"),api:api,_:_}
